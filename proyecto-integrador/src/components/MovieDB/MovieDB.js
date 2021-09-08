54279c5babacada89d89f2f49b9c8a8066a915b0
mateopizzolo <mpizzolo@udesa.edu.ar>
2021-09-08 08:04:33 -0300

    Empezando con el boton de cargar mas tarjetas

--- proyecto-integrador/src/components/MovieDB/MovieDB.js
+++ proyecto-integrador/src/components/MovieDB/MovieDB.js
@@ -2,55 +2,71 @@ import React, {Component} from 'react';
 import Carts from '../Carts/Carts'
 import Row from 'react-bootstrap/Row'
 
-
+const styleBoton = {
+    margin: 'auto',
+    textAlign: 'center',  
+    display: 'flex',
+    paddingTop: '10px',
+    paddingBottom: '10px',
+    marginTop: '25px',
+    marginBottom: '25px'
+}
 
 class MovieDB extends Component{
+
     constructor(){
         super();
         this.state = {
             peliculas:[],
             isLoaded: false,
+            urlKey: '?api_key=6d28a46fc010c27c72c60e718140556a',
+            urlPopularMovies: 'https://api.themoviedb.org/3/movie/popular',
+            nmrDePagina: 2
         }
     }
     componentDidMount(){
-        const key = '6d28a46fc010c27c72c60e718140556a'
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key='+key)
+        fetch(this.state.urlPopularMovies+this.state.urlKey)
             .then( response => response.json() )
             .then( data => {
                 console.log(data)
                 this.setState({
                     peliculas: data.results,
                     isLoaded: true,
-                    peliculasImagen: data.results
+                    peliculasImagen: data.results,
             }) 
             })
             .catch( error => console.log(error));
             
     }
-    ConfigurationMovie(){
-        const key = '6d28a46fc010c27c72c60e718140556a'
-        fetch('https://api.themoviedb.org/3/configuration?api_key='+key)
-            .then( response => response.json() )
+    addMorePopular(){
+        this.setState({
+            nmrDePagina: this.state.nmrDePagina + 1
+        }, () => console.log(this.state.peliculas))
+        fetch(this.state.urlPopularMovies+this.state.urlKey+'&page='+this.state.nmrDePagina)
+            .then( response => response.json())
             .then( data => {
-                console.log(data)
-                
+                console.log(data);
+                this.setState({
+                    peliculas: this.state.peliculas.concat(data.results),
+                 //   personajesIniciales: this.state.personajes.concat(data.results)
+                })
             })
-            .catch( error => console.log(error));
+            
     }
-      
+          
 
     render(){
         return(
+            <div>
+            <button type="button" style={styleBoton} onClick={() => this.addMorePopular()}>Cargar más tarjetas</button>
             <container>
                 <Row xs={1} md={5} className="g-4"> 
                     {
-                     this.state.peliculas.slice(0, 10).map( (characterData, idx) => <Carts data={characterData} key={characterData.name + idx}  />)
-                    }
-                    {this.ConfigurationMovie()
+                  this.state.peliculas.map( (characterData, idx) => <Carts data={characterData} key={characterData.name + idx}  />) 
                     }
                 </Row>
             </container>
-                            
+            </div>    
 
             
         )
