d6d77df0f637a20f0b4ea9bc86c05cc156f5983c
Nicolas Cavalieri <ncavalieri@udesa.edu.ar>
2021-09-14 20:59:37 -0300

    pequeñas correciones al footer y componente

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MovieDB from './components/MovieDB/MovieDB';
 import Disposicion from "./components/Disposicion/Disposicion.js";
 import Navbar from './components/NavigBar/NavigBar'
+import Footer from './components/Footer/Footer';
 
 //Estilos
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -22,26 +23,7 @@ function App() {
         </main>
         <br/>
         <br/>
-        <footer>
-            <ul className="teamFooter">
-                <li className="li-logo">
-                <a href="https://www.themoviedb.org/?language=es">
-
-                    <img src="/public/images/logoTMDb.png" alt="Logo TMDB"></img> 
-                    </a>
-                    </li>
-                <li className="li-footer">
-                <a href="https://ar.linkedin.com/in/serena-mar%C3%ADa-dominguez-a48368146"> Serena Dominguez </a>
-                     </li>
-                <li className="li-footer">
-                <a href="https://www.themoviedb.org/?language=es"> Mateo Pizzolo</a>
-                   </li>
-                <li className="li-footer">
-                <a href="https://www.themoviedb.org/?language=es"> Nicolas Cavalieri</a>
-                    </li>
-            </ul>
-        </footer>
-
+        <Footer/>
     </div>
   );
 }
---
+++ proyecto-integrador/src/components/Footer/Footer.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import './Footer.css'
+
+class Footer extends Component {    
+    render() {
+    return(
+        <footer>
+        <ul className="teamFooter">
+            <li className="li-logo">
+            <a href="https://www.themoviedb.org/?language=es">
+
+                <img className="logo" src="/images/logoTMDb.png" alt="Logo TMDB"></img> 
+                </a>
+                </li>
+            <li className="li-footer">
+            <a href="https://ar.linkedin.com/in/serena-mar%C3%ADa-dominguez-a48368146"> Serena Dominguez </a>
+                 </li>
+            <li className="li-footer">
+            <a href="#"> Mateo Pizzolo</a>
+               </li>
+            <li className="li-footer">
+            <a href="#"> Nicolas Cavalieri</a>
+                </li>
+        </ul>
+        </footer>        
+    )
+  }
+
+}
+
+
+export default Footer;
